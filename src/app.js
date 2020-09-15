2359f87cd39cd3fb10f402ddedcc3cc998d71bec
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-09-15 22:43:23 +0200

    Login component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import { useLang, useMeta, useTitle } from 'hooked-head/preact';
+import { useState } from 'preact/hooks';
+import { Login } from './components/login';
 
 const App = (() => {
   const appTitle = 'Piwny ranking Browar.biz';
   useLang('pl');
   useTitle(appTitle);
   useMeta({ name: 'author', content: 'Jarek Zgoda' });
+  const [token, setToken] = useState('');
   return (
-    <div id="app">
+    <div id="app" class="container">
+      <Login token={token} setToken={setToken} />
       <h1>{appTitle}</h1>
+      <div class="row">
+        <div class="column">
+          <h2>Najnowsze recenzje</h2>
+        </div>
+        <div class="column">
+          <h2>Najpopularniejsze piwa</h2>
+        </div>
+      </div>
     </div>
   )
 });
---
+++ src/components/login.js
@@ -0,0 +1,14 @@
+const Login = (({ token, setToken }) => {
+  if (token) {
+    
+  }
+  setToken('aaa');
+
+  return (
+    <div>
+      Login
+    </div>
+  );
+});
+
+export { Login };
